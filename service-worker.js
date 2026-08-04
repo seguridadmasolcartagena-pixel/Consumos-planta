5076c3aa4181c40f2fdce90baf15ae9afcfb7f09
seguridadmasolcartagena-pixel <seguridadmasolcartagena@gmail.com>
2026-08-04 13:22:07 +0200

    Actualizar cache para distribuir el nuevo formulario

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "masol-lecturas-v1";
+const CACHE_NAME = "masol-lecturas-v2";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
