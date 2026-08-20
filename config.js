7496f8c6218c8479864fa82ca18063fcc87b6bae
seguridadmasolcartagena-pixel <seguridadmasolcartagena@gmail.com>
2026-08-20 09:48:47 +0200

    Revertir URL del flujo del PR #21
    Restaura la configuración anterior de Power Automate.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MASOL_CONFIG = Object.freeze({
-  FLOW_URL: "https://default65afa47b9e4e4ad28cfe30d4118f06.2e.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/09/workflows/4538411fd9804cb5be13bd2f9a83fb20/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=PIZvH7YOX01aprNG2cuWJNFtH4VzKbffsef38my0tFM",
+  FLOW_URL: "https://default65afa47b9e4e4ad28cfe30d4118f06.2e.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/31/workflows/c38497ebb4794914a1bbc9c42b45b3b5/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=TNIPBnh8dmVqkYELygK9BlHS9-7zpzcaDOdchlqOaog",
   AUTO_SAVE_DELAY_MS: 1400
 });
